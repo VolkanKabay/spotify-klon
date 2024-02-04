46c6a466278e2dc146e1414a77068e3905185c00
VolkanKabay <124468930+VolkanKabay@users.noreply.github.com>
2024-02-04 17:20:32 +0100

    .

--- src/MusicAppBar.tsx
+++ src/MusicAppBar.tsx
@@ -1,4 +1,6 @@
-import { AppBar } from "@mui/material";
+import { Favorite } from "@mui/icons-material";
+import { AppBar, Box, Typography } from "@mui/material";
+import Image from "mui-image";
 
 export function MusicAppBar() {
   return (
@@ -10,6 +12,39 @@ export function MusicAppBar() {
         position: "fixed",
         background: "#111111",
       }}
-    ></AppBar>
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "start",
+          alignItems: "start",
+          flexDirection: "column",
+          position: "fixed",
+          left: "5.5%",
+          bottom: "3%",
+        }}
+      >
+        <Image
+          src="/images/sweater-weather-cover.jpg"
+          alt="Sweater Weather by the Neighbourhood"
+          style={{
+            height: "auto",
+            width: "60px",
+            position: "fixed",
+            left: "1.5%",
+            bottom: "2.5%",
+            transform: "none",
+          }}
+        />
+        <Typography>Sweater Weather</Typography>
+        <Typography color={"darkgray"} fontSize={"13px"}>
+          The Neighbourhood
+        </Typography>
+      </Box>
+      <Favorite
+        fontSize="small"
+        sx={{ position: "fixed", bottom: "4%", left: "13%" }}
+      />
+    </AppBar>
   );
 }
