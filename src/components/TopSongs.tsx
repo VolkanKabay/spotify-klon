3be63b5817da6a518582cd184d2deebe2e4c076d
VolkanKabay <124468930+VolkanKabay@users.noreply.github.com>
2024-02-08 17:27:13 +0100

    Update TopSongs.tsx

--- src/components/TopSongs.tsx
+++ src/components/TopSongs.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useStateProvider } from "../utils/StateProvider";
 import { reducerCases } from "../utils/Constants";
 import axios from "axios";
